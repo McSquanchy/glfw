45cb8a251c96319792a520a209e1be5321919e18
Kevin Buman <kevin.buman@students.fhnw.ch>
2022-04-03 15:59:03 +0200

    updated .gitignore to ignore bin and bin-int / updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -29,6 +29,31 @@ project "GLFW"
         "src/null_window.c",
         "src/null_joystick.c",
     }
+    
+    filter "system:linux"
+    pic "On"
+
+    systemversion "latest"
+    staticruntime "On"
+
+    files
+    {
+        "src/x11_init.c",
+        "src/x11_monitor.c",
+        "src/x11_window.c",
+        "src/xkb_unicode.c",
+        "src/posix_time.c",
+        "src/posix_thread.c",
+        "src/glx_context.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c",
+        "src/linux_joystick.c"
+    }
+
+    defines
+    {
+        "_GLFW_X11"
+    }
 
     filter "system:windows"
         systemversion "latest"
@@ -55,5 +80,10 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
         }
 
-    filter { "system:windows", "configurations:Release"}
-        buildoptions "/MT"
+        filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "on"
